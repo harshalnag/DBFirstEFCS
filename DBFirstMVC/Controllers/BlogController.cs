f22bd0ed7bfe08ae0afd68119f0739ab759f8a3d
harshalnag <harshalnag@live.com>
2014-02-03 14:39:55 -0500

    test

---
+++ DBFirstMVC/Controllers/BlogController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBFirstMVC.Models;
+
+
+namespace DBFirstMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            using (var db = new BlogDataEntities())
+            {
+                return View(db.Blogs.ToList());
+            }
+
+        }
+
+        //
+        // GET: /Blog/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Blog/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Blog/Create
+
+        [HttpPost]
+        public ActionResult Create(Blog blog)
+        {
+            try
+            {
+                using (var db = new BlogDataEntities())
+                {
+                    db.Blogs.Add(blog);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        //
+        // GET: /Blog/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new BlogDataEntities())
+            {
+                return View(db.Blogs.Find(id));
+            }
+
+        }
+
+        //
+        // POST: /Blog/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Blog blog)
+        {
+            try
+            {
+                using (var db = new BlogDataEntities())
+                {
+                    db.Entry(blog).State = System.Data.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        //
+        // GET: /Blog/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            using (var db = new BlogDataEntities())
+            {
+                return View(db.Blogs.Find(id));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Blog blog)
+        {
+            try
+            {
+                using (var db = new BlogDataEntities())
+                {
+                    db.Entry(blog).State = System.Data.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,8 @@
+This example accompanies 
+
+Building an MVC 3 App with Database First and Entity Framework 4.1
+
+Whitepaper: http://msdn.microsoft.com/en-us/data/gg685489
+
+Video: http://msdn.microsoft.com/en-us/data/gg702905
+
